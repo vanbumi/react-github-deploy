cb1705446cbafb035825f1d774dd8aeeba0e0069
vanbumi <jsp.dio@gmail.com>
2020-07-16 15:20:18 +0700

    Updates

---
+++ precache-manifest.9bd16ec99e9b4c8c868b4945af34cc60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a637e8e8744196a78df78a3506cd0eed",
+    "url": "/react-github-deploy/index.html"
+  },
+  {
+    "revision": "32a2d4a4e96c1e5fd0a7",
+    "url": "/react-github-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/react-github-deploy/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-deploy/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a2d4a4e96c1e5fd0a7",
+    "url": "/react-github-deploy/static/js/main.147cdc8f.chunk.js"
+  },
+  {
+    "revision": "456eedb0b8e14f37d91e",
+    "url": "/react-github-deploy/static/js/runtime-main.7b79c7a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
